dce0ddb620f474df4ce8784bc3e5a7b79dbc0bd4
Buzgau Catalin <buzgaucatalin@yahoo.com>
2023-07-04 13:14:03 +0300

    Check logins from file and bypass passwords

--- hack.py
+++ hack.py
@@ -1,10 +1,19 @@
 import socket
 import sys
-import os
+import string
+import json
 from itertools import product
 
 ip_address = sys.argv[1]
 port = sys.argv[2]
+letters_digits = string.ascii_letters + string.digits
+
+
+def create_generator(input_lst: list[str]) -> list[str]:
+    for login in input_lst:
+        message_generator = list(map(lambda x: ''.join(x),
+                                     product(*([letter.lower(), letter.upper()] for letter in login))))
+        return message_generator
 
 
 class Socket:
@@ -20,17 +29,36 @@ class Socket:
     def receive(self):
         return self.sock.recv(1024)
 
-    def check_password(self):
-        with open(f'{os.getcwd()}\\passwords.txt', 'r') as file:
-            passwords = file.read().splitlines()
-            for password in passwords:
+    def check_login(self):
+        with open('C:\\Python\\HyperSkill\\Password Hacker (Python)\\Password Hacker (Python)\\task\\hacking\\logins.txt', 'r') as file:
+            logins = file.read().splitlines()
+            for login in logins:
                 message_generator = list(map(lambda x: ''.join(x),
-                                             product(*([letter.lower(), letter.upper()] for letter in password))))
+                    product(*([letter.lower(), letter.upper()] for letter in login))))
 
                 for message in message_generator:
-                    self.sock.send(''.join(message).encode())
-                    if self.receive().decode('utf-8') == "Connection success!":
+                    message_to_json = json.dumps({"login": ''.join(message), "password": ' '})
+                    self.sock.send(message_to_json.encode())
+                    message_from_json = json.loads(self.receive().decode('utf-8'))
+                    if message_from_json["result"] == "Wrong password!":
                         return ''.join(message)
+
+    def check_password(self):
+        password_guess = ''
+        login = self.check_login()
+        generator = (letter for letter in letters_digits)
+        while True:
+            letter = next(generator)
+            password_guess += letter
+            message_to_json = json.dumps({"login": login, "password": password_guess})
+            self.sock.send(message_to_json.encode())
+            message_from_json = json.loads(self.receive().decode('utf-8'))
+            if message_from_json["result"] == "Exception happened during login":
+                generator = iter(letters_digits)
+            elif message_from_json["result"] == "Wrong password!":
+                password_guess = password_guess[:-1]
+            elif message_from_json["result"] == "Connection success!":
+                return message_to_json
 
     def close_conn(self):
         self.sock.close()
